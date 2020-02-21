ae0a2ae00b75588e794060bac2a28566e7a4037b
MagalieLindaChetrit <magaliechetrit@gmail.com>
2020-02-21 12:50:18 +0100

    index php has a loop

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php get_header(); ?>
+
+
+<main id="site-content" role="main">
+
+    <?php
+        if ( have_posts() ) {
+
+            // Load posts loop.
+            while ( have_posts() ) {
+                the_post();
+            }
+
+        } else {
+
+
+
+        }
+        ?>
+
+</main>
+
+
 <?php get_footer(); ?>
